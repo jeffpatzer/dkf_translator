98c6e2221bd2f9625a22b8d7ac5432a1bbe9c2b9
Jeff Patzer <jeff.patzer@moovweb.com>
2012-02-04 16:54:10 -0800

    project generation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DkfTranslator::Application.config.secret_token = 'eac374a8a7dbfbd2428d4136bf7770e995eb8715ce06afaa40713b4cb9e126224fc81f915a739f79486ce42d729c00804e01e83997a0acb2172b8765fbe03a84'
